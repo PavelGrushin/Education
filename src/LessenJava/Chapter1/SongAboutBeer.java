8ccdb424b11158ec1d9e36f968e01a26a91e940b
ChaserPavel <Chaser1989@mail.ru>
2022-05-17 13:55:31 +0300

    Небольшая песенка про бутылки Пива ст.44 (Исправлен текс песни)

--- src/LessenJava/Chapter1/SongAboutBeer.java
+++ src/LessenJava/Chapter1/SongAboutBeer.java
@@ -4,19 +4,23 @@ public class SongAboutBeer {
     public static void main(String[] args) {
         beerSong();
     }
+
     public static void beerSong() {
-        int beerNum = 99;
+        int beerNum = 10;
         String word = "Бутылок";
         while (beerNum > 0) {
-            if (beerNum == 1) {
-                word = "Бутылка";
-            }
+
             System.out.println(beerNum + " " + word + " пива на стене...");
             System.out.println(beerNum + " " + word + " пива.");
             System.out.println("Возьми одну.");
             System.out.println("Пусти по кругу.");
             beerNum--;
-  if(beerNum == 0) {
+            if (beerNum == 1) {
+                word = "Бутылка";
+            }
+            if (beerNum > 0) {
+                System.out.println(beerNum + " " + word + " пива на стене!");
+            } else {
                 System.out.println("Нет бутылок пива на стене!");
             }
         }
